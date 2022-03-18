db80b89bdd2dcca14ee720f4dfa4a61561c22094
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-03-17 22:32:56 -0300

    test: create mock for bcrypt

--- src/infra/criptography/bcypt-adapter.spec.ts
+++ src/infra/criptography/bcypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash_doido'
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const sut = new BcryptAdapter(12)
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  test('Should return a hash on success', async () => {
+    const sut = new BcryptAdapter(12)
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hash_doido')
   })
 })
